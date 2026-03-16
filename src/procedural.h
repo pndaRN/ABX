88b61c2bd56a94f4383bbfdcdc63a5a24c3f299d
Mark Williams <pndaRN.dev@proton.me>
2026-03-16 18:23:25 -0500

    add threshold varible to proc

--- src/procedural.h
+++ src/procedural.h
@@ -3,7 +3,7 @@
 
 typedef struct {
     int total_enemies, max_simult_divers; 
-    float speed, spawn_delay, dive_delay;
+    float speed, spawn_delay, dive_delay, threshold;
 } WaveParams;
 
 WaveParams level_to_params(int level);
